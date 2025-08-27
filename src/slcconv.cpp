c75fda8ebb798f43f55f62d1fe548f8af5d89fc7
peony <54771118+peonii@users.noreply.github.com>
2025-08-27 14:17:14 +0200

    converteur

--- src/slcconv.cpp
+++ src/slcconv.cpp
@@ -1,4 +1,6 @@
 #include "slc/formats/v2.hpp"
+#include "slc/formats/v3/atom.hpp"
+#include "slc/formats/v3/builtin.hpp"
 #include <chrono>
 #include <filesystem>
 #include <fstream>
@@ -17,21 +19,11 @@ template <class... Ts> struct overloads : Ts... {
   using Ts::operator()...;
 };
 
-int main() {
-  std::string inputName;
-  std::cout << "input file name: ";
-  std::cin >> inputName;
-
-  std::string outputName;
-  std::cout << "output file name: ";
-  std::cin >> outputName;
-  std::cout << "\n\n";
-
+static void convertSlc2ToSlc3(std::string inputName, std::string outputName) {
   const fs::path in_path = fs::current_path() / inputName;
   size_t oldSize = fs::file_size(in_path);
   std::ifstream in(in_path, std::ios::binary);
   auto oldrep = slc::v2::Replay<OldMeta>::read(in);
-
   if (oldrep.has_value()) {
     auto rr = oldrep.value();
 
@@ -89,7 +81,7 @@ int main() {
       std::ofstream fd(out_path, std::ios::binary);
       if (auto result = r.write(fd); !result.has_value()) {
         std::println("failed to write: {}", result.error().m_message);
-        return 1;
+        return;
       }
     }
 
@@ -170,5 +162,76 @@ int main() {
     }
   } else {
     std::println("exited with {}", static_cast<int>(oldrep.error()));
+  }
+}
+
+static void convertSlc3ToSlc2(std::string inputName, std::string outputName) {
+  const fs::path in_path = fs::current_path() / inputName;
+  // size_t oldSize = fs::file_size(in_path);
+  std::ifstream in(in_path, std::ios::binary);
+  auto oldrep = slc::v3::Replay<>::read(in);
+  if (oldrep.has_value()) {
+    auto rr = oldrep.value();
+
+    auto it = std::find_if(
+        rr.m_atoms.m_atoms.begin(), rr.m_atoms.m_atoms.end(), [](auto &v) {
+          return std::visit(
+              [](auto &atom) { return atom.id == slc::v3::AtomId::Action; }, v);
+        });
+
+    slc::v3::ActionAtom atom = std::get<slc::v3::ActionAtom>(*it);
+
+    std::println("read slc3 replay; {} inputs", atom.length());
+
+    std::println("------------------------------------");
+
+    slc::v2::Replay<OldMeta> r;
+
+    r.m_tps = oldrep->m_meta.m_tps;
+    for (size_t i = 0; i < atom.m_actions.size(); i++) {
+      const auto &a = atom.m_actions[i];
+
+      if (a.m_type == slc::v3::Action::ActionType::TPS) {
+        (void)r.addTPSInput(a.m_frame, a.m_tps);
+      } else {
+        (void)r.addInput(a.m_frame,
+                         static_cast<slc::v2::Input::InputType>(a.m_type),
+                         a.m_player2, a.m_holding);
+      }
+    }
+
+    r.m_meta.seed = rr.m_meta.m_seed;
+
+    const fs::path out_path = fs::current_path() / outputName;
+    {
+      std::println("writing slc2 replay...");
+      std::ofstream fd(out_path, std::ios::binary);
+      r.write(fd);
+    }
+
+  } else {
+    std::println("exited with {}", oldrep.error().m_message);
+  }
+}
+
+int main() {
+  std::string inputName;
+  std::cout << "input file name: ";
+  std::cin >> inputName;
+
+  std::string outputName;
+  std::cout << "output file name: ";
+  std::cin >> outputName;
+  std::cout << "\n\n";
+
+  int mode;
+  std::cout << "Mode (0 - slc2 to slc3, 1 - slc3 to slc2): ";
+  std::cin >> mode;
+  std::cout << "\n\n";
+
+  if (mode == 1) {
+    convertSlc3ToSlc2(inputName, outputName);
+  } else {
+    convertSlc2ToSlc3(inputName, outputName);
   }
 }
