da54a5a7bb48adf81a38ed64745d4a9573e0bb27
peony <54771118+peonii@users.noreply.github.com>
2025-12-16 15:26:58 +0100

    feat: variance algorithm in metadata, bump slc3 version number

--- include/slc/formats/v3/metadata.hpp
+++ include/slc/formats/v3/metadata.hpp
@@ -16,10 +16,19 @@ struct Metadata {
   double m_tps;
   uint64_t m_seed;
 
-  uint32_t m_version = 1;
+  uint32_t m_version = 2;
   uint32_t m_build;
 
-  char __padding[40] = {0};
+  /**
+   * What randomness algorithm to use for variance.
+   *
+   * 0 = Current algorithm - uses array indices in trigger instance and random
+   * seed to determine variance index. Generates variance values based on random
+   * seed.
+   */
+  uint32_t m_randomnessAlgorithm = 0;
+
+  char __padding[36] = {0};
 };
 
 static_assert(sizeof(Metadata) == METADATA_SIZE);
